3927bd62debd0e9174c4925380de02c29d7275c2
patolord <rodrigo_luizp@hotmail.com>
2025-10-16 08:50:04 -0300

    feat: update site title and improve header component to display multiple title formats, enhance mobile theme toggle functionality

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Rodrigo's Personal Site";
+export const SITE_TITLE = "Rodrigo Luiz Schil Pellegrini";
+export const SITE_TITLE_SHORT = "Rodrigo L. S. Pellegrini";
+export const SITE_TITLE_INITIALS = "Rodrigo L. S. P.";
 export const SITE_DESCRIPTION = "Welcome to my website!";
